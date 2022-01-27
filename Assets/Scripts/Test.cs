803fc43068129d1c900ea46785120e6972abaecf
carrotpie <carrotpie@gmail.com>
2022-01-27 21:06:03 +0200

    Empty project

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    void Start() {
+        Debug.Log("test");
+    }
+
+    void Update() {
+
+    }
+}
