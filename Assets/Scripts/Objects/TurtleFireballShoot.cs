77c836ddfa5b7ab3bdbc448095851e761f22f630
Qua <analizatoriuslx@gmail.com>
2022-01-30 14:02:18 +0200

    added sounds/phrases

--- Assets/Scripts/Objects/TurtleFireballShoot.cs
+++ Assets/Scripts/Objects/TurtleFireballShoot.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class TurtleFireballShoot : MonoBehaviour {
 
+    Turtle turtle;
     public GameObject effect;
     public Transform source;
 
     private void Start() {
+        if (turtle == null) turtle = GetComponent<Turtle>();
         EventCoordinator.StartListening(EventName.Input.Fireball(), Shoot);
     }
 
